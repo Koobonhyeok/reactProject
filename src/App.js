d2a934ab6bea178b6848e1300e6a98846c732541
koo <korea6441@gmail.com>
2021-12-04 17:10:02 +0900

    commit data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    Message : 'Message Memo'
+  }
+  changeMessage = () =>{
+    this.setState({
+      Message : 'Change Message'
+    })
+  }
+  render(){
+    return <div>
+            <p>{this.state.Message}</p>
+            <button onClick={this.changeMessage}>Click Alert</button>
+            </div>
+  }
 }
 
 export default App;
