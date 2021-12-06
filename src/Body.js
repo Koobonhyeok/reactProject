ad9fbf3095168589ec75e04cdb1e49f23ccc195c
koobonhyeok <korea6441@gmail.com>
2021-12-06 20:50:05 +0900

    rocomo Commit

---
+++ src/Body.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Body extends Component{
+    render() {
+        return <div><p>test</p></div>
+    }
+}
+
+export default Body;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Boby from './Body'; 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(  <App />,  document.getElementById('root') );
+ReactDOM.render(  <Boby />,  document.getElementById('root1') );
 
