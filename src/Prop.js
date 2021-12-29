745b5a60d2f851b19934867277a0f0aef6647e82
koobonhyeok <korea6441@gamil.com>
2021-12-29 23:07:16 +0900

    axios commit

--- src/Prop.js
+++ src/Prop.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
+import Axios from "./components/dev/Axios";
 
 class Prop extends Component{
     render(){
         return (
-            <div className={this.props.text}><p> 안녕하세요. {this.props.text} 테스트 입니다 </p></div>
+            <Axios />
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import Prop from './Prop';
 import './css/RCA.css'
 
 ReactDOM.render(  
     <App />
+    // <Prop />
     ,  document.getElementById('root') );
